0c839fbba531cdda1699b1602613ad04fbc8d742
Aaron Kiser <aaronkiser88@gmail.com>
2018-09-27 10:29:05 -0400

    added SpriteSheet and TileSet

---
+++ GFX/SpriteSheet.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PrototypeGame2.GFX
+{
+    public class SpriteSheet
+    {
+        Game1 game1;
+        Texture2D spriteSheet;
+
+        private int spriteSheetWidth;
+        private int spriteSheetHeight;
+        private int spriteWidth;
+        private int spriteHeight;
+
+        public SpriteSheet(Game1 game1)
+        {
+            this.game1 = game1;
+        }
+
+        public void Load(string path)
+        {
+            spriteSheet = game1.Content.Load<Texture2D>(path);
+            spriteSheetWidth = spriteSheet.Width;
+            spriteSheetHeight = spriteSheet.Height;
+        }
+
+        public Rectangle SpriteView(int frame, int layer)
+        {
+            int totalNumberOfFrames = 3;
+            int totalNumberOfLayers = 1;
+
+            int spriteFrameWidth = spriteWidth = spriteSheetWidth / totalNumberOfFrames;
+            int spriteFrameHeight = spriteHeight = spriteSheetHeight / totalNumberOfLayers;
+
+            int spriteFrameX = (spriteSheetWidth / totalNumberOfFrames) * (frame - 1);
+            int spriteFrameY = (spriteSheetHeight / totalNumberOfLayers) * (layer - 1);
+
+            return new Rectangle(spriteFrameX, spriteFrameY, spriteFrameWidth, spriteFrameHeight);
+        }
+
+
+        public int getSpriteSheetWidth() { return spriteSheetWidth; }
+        public int getSpriteSheetHeight() { return spriteSheetHeight; }
+        public int getSpriteWidth() { return spriteWidth; }
+        public int getSpriteHeight() { return spriteHeight; }
+    }
+}
---
+++ GFX/Tileset.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Xml;
+
+namespace PrototypeGame2
+{
+    public class Tileset
+    {
+        string orientation, renderOrder, tileLayer;
+        int width, height, tileWidth, tileHeight, infinite, nextObjectID, tileLayerWidth, tileLayerHeight;
+        int[] tileArray;
+
+        public Tileset(string path)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Content/XML/Levels/" + path + ".xml");
+
+            orientation = getAttributeValue(xmlDoc, "map", "orientation");
+            renderOrder = getAttributeValue(xmlDoc, "map", "renderorder");
+            width = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "width"));
+            height = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "height"));
+            tileWidth = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "tilewidth"));
+            tileHeight = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "tileheight"));
+            infinite = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "infinite"));
+            nextObjectID = System.Convert.ToInt32(getAttributeValue(xmlDoc, "map", "nextobjectid"));
+            tileLayer = getAttributeValue(xmlDoc, "layer", "name");
+            tileLayerWidth = System.Convert.ToInt32(getAttributeValue(xmlDoc, "layer", "width"));
+            tileLayerHeight = System.Convert.ToInt32(getAttributeValue(xmlDoc, "layer", "height"));
+            tileArray = dataArray(xmlDoc, xmlDoc.GetElementsByTagName("data"));
+        }
+
+        private string getAttributeValue(XmlDocument xmlDoc, string element, string attribute)
+        {
+            XmlNodeList map = xmlDoc.GetElementsByTagName(element);
+            for (int i = 0; i < map.Count; i++)
+            {
+                for (int j = 0; j < map[i].Attributes.Count; j++)
+                {
+                    if (map[i].Attributes[j].Name.Equals(attribute))
+                    {
+                        return map[i].Attributes[j].Value;
+                    }
+                }
+            }
+            return null;
+        }
+        private int[] dataArray(XmlDocument xmlDoc, XmlNodeList xmlNode)
+        {
+            int count = 0;
+
+            for (int i = 0; i < xmlNode.Count; i++)
+            {
+                for (int k = 0; k < xmlNode[i].Attributes.Count; k++)
+                {
+                    for (int j = 0; j < xmlNode[i].FirstChild.Value.Length; j++)
+                    {
+                        if (isDigit(xmlNode[i].FirstChild.Value[j]))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            int[] tileArray = new int[count];
+
+            count = 0;
+            for (int i = 0; i < xmlNode.Count; i++)
+            {
+                for (int k = 0; k < xmlNode[i].Attributes.Count; k++)
+                {
+                    for (int j = 0; j < xmlNode[i].FirstChild.Value.Length; j++)
+                    {
+                        if (isDigit(xmlNode[i].FirstChild.Value[j]))
+                        {
+                            tileArray[count] = (int)char.GetNumericValue(xmlNode[i].FirstChild.Value[j]);
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return tileArray;
+        }
+        private bool isDigit(char x)
+        {
+            
+            string num = "1234567890";
+            bool digit = false;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (x.Equals(num[i]))
+                {
+                    digit = true;
+                    break;
+                }
+                else
+                {
+                    digit = false;
+                }
+            }
+
+            return digit;
+        }
+
+        public int getWidth() { return width; }
+        public int getHeight() { return height; }
+        public int getTileWidth() { return tileWidth; }
+        public int getTileHeight() { return tileHeight; }
+        public bool isInfinite()
+        {
+            if(infinite == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int getNextObjectID() { return nextObjectID; }
+        public int[] getTileArray() { return tileArray; }
+        public string getTileLayer() { return tileLayer; }
+        public int getTileLayerWidth() { return tileLayerWidth; }
+        public int getTileLayerHeight() { return tileLayerHeight; }
+    }
+}
